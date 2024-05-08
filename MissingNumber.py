d3e83331782590e575cdb5c3e9844e5def8cebf4
Noah Ybarra <nybarra.engr@gmail.com>
2024-05-08 17:11:55 -0500

    Create MissingNumber.py

---
+++ MissingNumber.py
@@ -0,0 +1,5 @@
+# This program will take a list of N number of elements.
+# The list will hold all of the elements from 1 to N in no particular order.
+# One number will be missing and it is up to the program to determine the missing number.
+# For example, N = 5 input: [1, 2, 5, 4], output: 3
+# HINT: N is the highest index + 1.
