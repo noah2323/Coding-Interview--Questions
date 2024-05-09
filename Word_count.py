cd7a018e5970cedabf7108f707b9f582fb7219f1
Noah Ybarra <nybarra.engr@gmail.com>
2024-05-09 17:27:02 -0500

    Create Word_count.py

---
+++ Word_count.py
@@ -0,0 +1,9 @@
+# Question: Implement a function called word_count that takes a string as input and returns a dictionary 
+# where the keys are unique words in the string and the values are the frequencies of those words. 
+# Assume that words are separated by spaces and that capitalization should be ignored.
+
+# For example:
+
+# Input: "The quick brown fox jumps over the lazy dog."
+# Output: {'the': 2, 'quick': 1, 'brown': 1, 'fox': 1, 'jumps': 1, 'over': 1, 'lazy': 1, 'dog.': 1}
+
