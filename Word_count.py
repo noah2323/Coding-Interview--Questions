002e311cf41e46f115ef6ea4798f4cdd042ca0af
Noah Ybarra <nybarra.engr@gmail.com>
2024-05-09 18:20:30 -0500

    Update Word_count.py

--- Word_count.py
+++ Word_count.py
@@ -7,3 +7,6 @@
 # Input: "The quick brown fox jumps over the lazy dog."
 # Output: {'the': 2, 'quick': 1, 'brown': 1, 'fox': 1, 'jumps': 1, 'over': 1, 'lazy': 1, 'dog.': 1}
 
+inputstr = str(input("Type in phrase: "))
+print(inputstr)
+
